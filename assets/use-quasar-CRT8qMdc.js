e9b9f24e3854ce328fb516ba6e465965dd3a0c22
Jan Krys <pichalek@gmail.com>
2025-08-01 22:44:27 +0200

    fix: dummy users friend requests

---
+++ assets/use-quasar-CRT8qMdc.js
@@ -0,0 +1,1 @@
+import{G as a,bc as r}from"./index-Cgi22ZXB.js";function u(){return a(r)}export{u};
--- assets/use-quasar-DQXJwWie.js
+++
@@ -1,1 +0,0 @@
-import{G as a,bc as r}from"./index-DXWY3Ptu.js";function u(){return a(r)}export{u};
