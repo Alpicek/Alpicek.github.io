f186d64c06f58ea30da0282691590035960a539e
krys <krys@its.cz>
2025-08-05 14:47:01 +0200

    frix: friends loading

--- assets/auth-CkjZL2Nu.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./index-DsP-nMLo.js";import{u as a}from"./auth-store-Bf3hSUlm.js";import"./supabase-DBrb1RXq.js";const s=o(async({store:t})=>{await a(t).initializeAuth()});export{s as default};
---
+++ assets/use-quasar-B4nle0bC.js
@@ -0,0 +1,1 @@
+import{G as a,be as r}from"./index-DcT2ux80.js";function s(){return a(r)}export{s as u};
