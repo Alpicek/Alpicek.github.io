cea4ab41e4e9bf71a60b2f52deb63258a6f659fb
krys <krys@its.cz>
2025-08-08 14:20:14 +0200

    notification handling

--- assets/use-quasar-DTpZL-V_.js
+++
@@ -1,1 +0,0 @@
-import{G as a,bf as r}from"./index-CMeCmwpZ.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar-ItPRXWbW.js
@@ -0,0 +1,1 @@
+import{G as a,bg as r}from"./index-dYzdyrNV.js";function u(){return a(r)}export{u};
