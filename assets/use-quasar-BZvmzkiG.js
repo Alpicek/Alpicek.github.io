434ea939a691d364036ddc683f82c60aa4f2490d
krys <krys@its.cz>
2025-08-10 00:05:56 +0200

    auth callback, landing page

--- assets/use-quasar-BZvmzkiG.js
+++
@@ -1,1 +0,0 @@
-import{G as a,bg as r}from"./index-CIQSULin.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar-ruDJR1eq.js
@@ -0,0 +1,1 @@
+import{I as a,bg as r}from"./index-YeBCv6h1.js";function u(){return a(r)}export{u};
