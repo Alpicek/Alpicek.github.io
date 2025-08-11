2d020bfdc7860dca3655bcb8eaea6aa203fcffb5
Jan Krys <pichalek@gmail.com>
2025-08-11 10:32:10 +0200

    styling

---
+++ assets/use-quasar-DPWoZfwT.js
@@ -0,0 +1,1 @@
+import{I as a,bg as r}from"./index-COPe4EcT.js";function u(){return a(r)}export{u};
--- assets/use-quasar-ruDJR1eq.js
+++
@@ -1,1 +0,0 @@
-import{I as a,bg as r}from"./index-YeBCv6h1.js";function u(){return a(r)}export{u};
