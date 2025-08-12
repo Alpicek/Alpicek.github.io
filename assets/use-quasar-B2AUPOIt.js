32806cd7fc9eff91708b5535eb97163833b1dd0e
Jan Krys <pichalek@gmail.com>
2025-08-12 13:28:36 +0200

    wishes cards styling

---
+++ assets/use-quasar-B2AUPOIt.js
@@ -0,0 +1,1 @@
+import{I as a,bc as r}from"./index-BC9OHaAo.js";function u(){return a(r)}export{u};
--- assets/use-quasar-BotVfJR3.js
+++
@@ -1,1 +0,0 @@
-import{I as a,bg as r}from"./index-D27XNSYz.js";function u(){return a(r)}export{u};
