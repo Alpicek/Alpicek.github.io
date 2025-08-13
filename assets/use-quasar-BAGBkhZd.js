79c4325929047b4057ef7fcfb3a57162a3a3914c
Jan Krys <pichalek@gmail.com>
2025-08-13 14:14:58 +0200

    groups expanding

---
+++ assets/use-quasar-BAGBkhZd.js
@@ -0,0 +1,1 @@
+import{I as a,be as r}from"./index-B3spGy88.js";function s(){return a(r)}export{s as u};
--- assets/use-quasar-Dhk6w8yX.js
+++
@@ -1,1 +0,0 @@
-import{I as a,bc as r}from"./index-DywPtqBD.js";function u(){return a(r)}export{u};
