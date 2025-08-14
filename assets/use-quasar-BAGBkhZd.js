2e61a210c9b7285be0e9ccc6e1e559e47d52fbbc
Jan Krys <pichalek@gmail.com>
2025-08-14 10:11:22 +0200

    GDPR

--- assets/use-quasar-BAGBkhZd.js
+++
@@ -1,1 +0,0 @@
-import{I as a,be as r}from"./index-B3spGy88.js";function s(){return a(r)}export{s as u};
---
+++ assets/use-quasar-BqfgzqmL.js
@@ -0,0 +1,1 @@
+import{L as a,be as r}from"./index-DY4q8G6M.js";function s(){return a(r)}export{s as u};
